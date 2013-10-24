3babf62121da0b5515d9eabfe2f1a407ba32b5ab
Mark Casias <mcasias@newsignature.com>
2013-10-24 15:00:11 -0700

    updating theme based on latest logo and colors.
    Signed-off-by: Mark Casias <mcasias@newsignature.com>

--- sites/all/themes/abqwebgeeks/templates/page.tpl.php
+++ sites/all/themes/abqwebgeeks/templates/page.tpl.php
@@ -9,44 +9,50 @@
 ?>
 
 <div id="page">
-
   <header class="header" id="header" role="banner">
-
-    <?php if ($logo): ?>
-      <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" rel="home" class="header__logo" id="logo"><img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" class="header__logo-image" /></a>
-    <?php endif; ?>
-
+    <a href="<?php print $front_page; ?>" title="ABQ Webgeeks" rel="home" class="header__logo" id="logo">
+      <img src="<?php print $logo; ?>" alt="ABQ Webgeeks" class="header__logo-image" />
+    </a>
     <?php if ($site_name || $site_slogan): ?>
-      <div class="header__name-and-slogan" id="name-and-slogan">
-      <?php if ($site_name): ?>
-        <h1 class="header__site-name" id="site-name">
-          <a href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>" class="header__site-link" rel="home"><span><?php print $site_name; ?></span></a>
-        </h1>
-      <?php endif; ?>
-        <?php if ($site_slogan): ?>
-          <div class="header__site-slogan" id="site-slogan"><?php print $site_slogan; ?></div>
+      <?php if ($site_slogan): ?>
+    <div class="header__site-slogan" id="site-slogan"><?php print $site_slogan; ?></div>
         <?php endif; ?>
-      </div>
     <?php endif; ?>
 
-    <?php if ($secondary_menu): ?>
-      <nav class="header__secondary-menu" id="secondary-menu" role="navigation">
-        <?php print theme('links__system_secondary_menu', array(
-          'links' => $secondary_menu,
+    <div id="navigation">
+      <nav id="main-menu" role="navigation" tabindex="-1">
+        <?php
+        print theme('links__system_main_menu', array(
+          'links' => $main_menu,
           'attributes' => array(
             'class' => array('links', 'inline', 'clearfix'),
           ),
           'heading' => array(
-            'text' => $secondary_menu_heading,
+            'text' => t('Main menu'),
             'level' => 'h2',
             'class' => array('element-invisible'),
           ),
         )); ?>
       </nav>
-    <?php endif; ?>
+      <?php print render($page['navigation']); ?>
+    </div>
 
+  <?php if ($secondary_menu): ?>
+    <nav class="header__secondary-menu" id="secondary-menu" role="navigation">
+      <?php print theme('links__system_secondary_menu', array(
+        'links' => $secondary_menu,
+        'attributes' => array(
+          'class' => array('links', 'inline', 'clearfix'),
+        ),
+        'heading' => array(
+          'text' => $secondary_menu_heading,
+          'level' => 'h2',
+          'class' => array('element-invisible'),
+        ),
+      )); ?>
+    </nav>
+  <?php endif; ?>
     <?php print render($page['header']); ?>
-
   </header>
 
   <div id="main">
@@ -70,32 +76,6 @@
       <?php print $feed_icons; ?>
     </div>
 
-    <div id="navigation">
-
-      <?php if ($main_menu): ?>
-        <nav id="main-menu" role="navigation" tabindex="-1">
-          <?php
-          // This code snippet is hard to modify. We recommend turning off the
-          // "Main menu" on your sub-theme's settings form, deleting this PHP
-          // code block, and, instead, using the "Menu block" module.
-          // @see https://drupal.org/project/menu_block
-          print theme('links__system_main_menu', array(
-            'links' => $main_menu,
-            'attributes' => array(
-              'class' => array('links', 'inline', 'clearfix'),
-            ),
-            'heading' => array(
-              'text' => t('Main menu'),
-              'level' => 'h2',
-              'class' => array('element-invisible'),
-            ),
-          )); ?>
-        </nav>
-      <?php endif; ?>
-
-      <?php print render($page['navigation']); ?>
-
-    </div>
 
     <?php
       // Render the sidebars to see if there's anything in them.
@@ -111,7 +91,6 @@
     <?php endif; ?>
 
   </div>
-
   <?php print render($page['footer']); ?>
 
 </div>
